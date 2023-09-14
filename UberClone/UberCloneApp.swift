77758d12aef40eb3022fcc19637bf77f030497bb
Damitha Raveendra <deamonsarea52@gmail.com>
2023-09-14 22:33:55 +0530

    First Commit
    # Adding initial Files to repo

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Damitha Raveendra on 2023-09-13.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

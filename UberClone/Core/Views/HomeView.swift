9ee2ba154304ed187a9913a394bc71e879430dee
Damitha Raveendra <deamonsarea52@gmail.com>
2023-09-15 18:45:24 +0530

    Organizing Initial Project
    # Create folder structure and home view for the start

---
+++ UberClone/Core/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  UberClone
+//
+//  Created by Damitha Raveendra on 2023-09-15.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}

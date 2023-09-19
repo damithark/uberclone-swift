3351af3305a7d4b95ac30e19de21fe1ab2e2ab61
Damitha Raveendra <deamonsarea52@gmail.com>
2023-09-20 00:18:42 +0530

    Code Cleanup
    # Remove print statement

--- UberClone/Core/LocationSearch/ViewModel/LocationSearchViewModel.swift
+++ UberClone/Core/LocationSearch/ViewModel/LocationSearchViewModel.swift
@@ -16,7 +16,6 @@ class LocationSearchViewModel: NSObject, ObservableObject {
     private let searchCompleter = MKLocalSearchCompleter()
     var queryFragment: String = "" {
         didSet {
-            print("DEBUG: Query fragment is \(queryFragment)")
             searchCompleter.queryFragment = queryFragment
         }
     }

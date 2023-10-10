088daa6c672be621185a1a2ac06c6701b8033e49
Damitha Raveendra <deamonsarea52@gmail.com>
2023-10-10 23:40:58 +0530

    Error Fix
    # Fixed implementation errors

--- UberClone/Core/Home/Views/UberMapViewRepresentable.swift
+++ UberClone/Core/Home/Views/UberMapViewRepresentable.swift
@@ -58,6 +58,12 @@ extension UberMapViewRepresentable {
         
         @objc private func updateSelectedCoordinateWithNotification(_ notification: NSNotification) {
               print("notification: \(notification)")
+            if let userInfo = notification.userInfo as? NSDictionary
+                {
+                  if let progressValue = userInfo["coordinate"] as? CLLocationCoordinate2D {
+                      self.addAndSelectAnnotation(withCoordinate: progressValue)
+                  }
+                }
         }
         
         // MARK: - MKMapViewDelegate
